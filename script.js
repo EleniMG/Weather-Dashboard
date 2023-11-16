b4e299acdaca90eba4aed0fd4f9de318b094eb19
Eleni <elenimariex@gmail.com>
2023-11-16 17:04:02 +0000

    Adding trim() to user's input and appending all current weather variables to webpage

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // Create dashboard - to include search section, buttons of previously searched locations, current weather for search result and future weather
 // Connect weather API Key
 // When you press search a location or press a button, create a function so that the current and future weather conditions are displayed
+
 $('.search-button').on('click', function(event){
     event.preventDefault();
-    var usersCityEntry = $('.weather-search').val();
+    var usersCityEntry = $('.weather-search').val().trim();
 
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + usersCityEntry + "&appid=56e83c8f1e65cf3c5412e2ae1f8c1687";
 
     fetch(queryURL).then(function(response){
         return response.json();
-    }).then(function(data){
+    }).then(function (data){
         var cityName = data.city.name;
         var today = dayjs().format('dddd Do MMMM YYYY')
         var currentTemperature = (data.list[0].main.temp - 273.15).toFixed(2)
@@ -17,11 +18,13 @@ $('.search-button').on('click', function(event){
         var currentWeatherIcon = data.list[0].weather[0].icon;
         var currentHumidity = data.list[0].main.humidity + "%";
         var currentWindSpeed = data.list[0].wind.speed + "m/s";
+
+    $('#today').append(cityName, today, currentTemperature, currentWeatherCondition, currentWeatherIcon, currentHumidity, currentWindSpeed)
+
     })
 
+
 })
-
-
 
 
 
