78f5fe034c3a6fde7076bc5616341bba8443068b
Eleni <elenimariex@gmail.com>
2023-11-16 15:22:59 +0000

    Adding pseudocode

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create dashboard - to include search section, buttons of previously searched locations, current weather for search result and future weather
+
+// Create a weather conditions object to match a weather condition with the correpsonding icon
+
+// Add search result as a button
+
+// Add search result to local storage
+
+// Create a function to clear previously searched buttons
+
+// When you press search a location or press a button, create a function so that the current and future weather conditions are displayed
